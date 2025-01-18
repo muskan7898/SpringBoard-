4c44ebc535d2ff461823eb6fb59d2ba0406faf44
muskan7898 <muskannareliya7@gmail.com>
2025-01-18 22:43:40 +0530

    some mistakes

--- src/main/java/com/example/spring/board/repository/VehicleRepository.java
+++ src/main/java/com/example/spring/board/repository/VehicleRepository.java
@@ -2,7 +2,6 @@ package com.example.spring.board.repository;
 
 import com.example.spring.board.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
